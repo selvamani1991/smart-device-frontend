4077158ac5038748dd0f8bb0a674e92a9e2ad5d6
selvamanim <selvamani@itflexsolutions.com>
2021-03-03 11:43:49 +0530

    v1 2nd time files

---
+++ src/typings.d.ts
@@ -0,0 +1,8 @@
+/* SystemJS module definition */
+declare var module: NodeModule;
+interface NodeModule {
+  id: string;
+}
+declare var MarkerClusterer: any;
+//declare var $: any;
+//declare var jQuery: any;
